4c6fd0c923dbb371119551d41171ca024f53d0bc
luanpoppe <luanpoppe@gmail.com>
2025-07-29 15:22:49 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+
+void ExibirMensagemDeBoasVindas()
+{
+  Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝
+
+░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+  Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+  Console.WriteLine("Digite 1 para registrar uma banda");
+  Console.WriteLine("Digite 2 para mostra todas as bandas");
+  Console.WriteLine("Digite 3 para avaliar uma banda");
+  Console.WriteLine("Digite 4 para exibir a média de uma banda");
+  Console.WriteLine("Digite -1 para sair");
+}
+
+ExibirMensagemDeBoasVindas();
