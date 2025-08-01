f73cb0ce5840ae24673837afaeaad9daf9b25499
luanpoppe <luanpoppe@gmail.com>
2025-07-31 21:46:51 -0300

    feat: adicionando switch cases

--- Program.cs
+++ Program.cs
@@ -32,9 +32,23 @@ void ExibirOpcoesDoMenu()
   string opcaoEscolhida = Console.ReadLine()!;
   int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
 
-  if (opcaoEscolhidaNumerica == 1)
+  switch (opcaoEscolhidaNumerica)
   {
-    Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+    case 1:
+      Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+      break;
+    case 2:
+      Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+      break;
+    case 3:
+      Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+      break;
+    case 4:
+      Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+      break;
+    case -1:
+      Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+      break;
   }
 }
 
